9bd54994100f7909eccb3f959a5408625c7ae29e
techprocompany <79759786+techprocompany@users.noreply.github.com>
2021-03-13 09:22:07 -0800

    this file has been uploaded from techpro server in
    This is just the beginning

---
+++ assets/dist/animate-scroll-effect.js
@@ -0,0 +1,93 @@
+window.sr = ScrollReveal();
+// shadow-effect
+        sr.reveal('.shadow', {
+                duration: 3000,
+                reset: true,
+                distance: '600px',
+                // delay:  000,
+                origin: 'middle',
+                viewFactor: 0.5
+            });    
+        
+        sr.reveal('.heading-site', {
+                reset: true,
+                duration: 1000,
+                // delay: 3500,
+                origin: 'right',
+                distance: '200px'
+            });
+
+        sr.reveal('.site-content', {
+                duration: 3000,
+                reset: true,
+                // delay: 2000,
+                origin: 'bottom',
+                distance: '300px'
+            });
+
+        sr.reveal('.site-content-carousel', {
+                duration: 3000,
+                reset: true,
+                // delay: 2000,
+                origin: 'bottom',
+                distance: '200px'
+            });
+
+        sr.reveal('.site-content-carousel-sec-3', {
+                duration: 3000,
+                // delay: 2000,
+                reset: false,
+                origin: 'left',
+                distance: '300px',
+                viewFactor: 0.5
+                });
+
+        sr.reveal('.shadowEffect', {
+                duration: 2000,
+                reset: false,
+                origin: 'middle',
+                distance: '300px',
+                viewFactor: 0.2
+            });
+
+        sr.reveal('.shadowEffectTop', {
+                duration: 1000,
+                reset: false,
+                origin: 'right',
+                distance: '200px',
+                viewFactor: 0.3
+            });
+        sr.reveal('.shadowEffectBottom', {
+                duration: 2000,
+                origin: 'bottom',
+                distance: '200px',
+                // easing: 'ease',
+                reset: false,
+                // scale:    { direction: 'up', power: '5%' },
+                viewFactor: 0.3
+
+            });
+         sr.reveal('.shadowEffectTel', {
+                duration: 3000,
+                // delay: 2000,
+                reset: false,
+                origin: 'right',
+                distance: '100px',
+                viewFactor: 0.3
+            });
+          sr.reveal('.shadowEffectEmail', {
+                duration: 3000,
+                // delay: 3000,
+                reset: false,
+                origin: 'left',
+                distance: '100px',
+                viewFactor: 0.3
+            });
+            sr.reveal('.shadowEffectForm', {
+                duration: 3000,
+                // delay: 2000,
+                reset: false,
+                origin: 'left',
+                distance: '200px',
+                viewFactor: 0.5
+            });
